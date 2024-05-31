4a03603ef2432dd35675ddcfd7c5688fdc23235a
eomjinyoung <jinyoung.eom@gmail.com>
2024-05-31 17:36:27 +0900

    .

--- java-lang/app/src/main/java/study/lang/literal/Test04.java
+++ java-lang/app/src/main/java/study/lang/literal/Test04.java
@@ -1,22 +1,17 @@
 package study.lang.literal;
 
 // 실습
-// - 문자 A를 유니코드로 표현하라.
+// - 주어진 문자를 유니코드 literal로 변경하여 출력하라!
 //
 
 public class Test04 {
     public static void main(String[] args) {
         // 코드를 완성하라.
-        System.out.println('A'); // 문자 리터럴
-        System.out.println('\u0041'); // 유니코드 문자 리터럴
+        System.out.println('A');
         System.out.println('a');
-        System.out.println('\u0061'); // 유니코드 문자 리터럴
         System.out.println('1');
-        System.out.println('\u0031'); // 유니코드 문자 리터럴
         System.out.println('가');
-        System.out.println('\uac00'); // 유니코드 문자 리터럴
         System.out.println('똥');
-        System.out.println('\ub625'); // 유니코드 문자 리터럴
-        System.out.println('\u00a9'); // copyright 문자를 유니코드 문자 리터럴로 표현
+        System.out.println(/* copyrights 문자 */);
     }
 }
