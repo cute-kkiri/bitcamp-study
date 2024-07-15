2f4a9cdcc66b0014ed5c80c095e5f06ddbb116f0
eomjinyoung <jinyoung.eom@gmail.com>
2024-07-15 11:27:31 +0900

    .

--- myapp/app/src/main/java/bitcamp/menu/MenuItem.java
+++ myapp/app/src/main/java/bitcamp/menu/MenuItem.java
@@ -1,33 +1,13 @@
 package bitcamp.menu;
 
-import java.util.Objects;
-
-public class MenuItem implements Menu {
-
-  private String title;
+public class MenuItem extends AbstractMenu {
 
   public MenuItem(String title) {
-    this.title = title;
+    super(title);
   }
 
   @Override
   public void execute() {
     System.out.println(title);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof MenuItem menuItem)) {
-      return false;
-    }
-    return Objects.equals(title, menuItem.title);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(title);
   }
 }
