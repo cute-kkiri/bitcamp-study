701601f1ed05271f8d97878fa0821e994a62744a
eomjinyoung <jinyoung.eom@gmail.com>
2024-07-29 12:52:56 +0900

    .

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/dao/skel/ResponseStatus.java
@@ -0,0 +1,8 @@
+package bitcamp.myapp.dao.skel;
+
+public class ResponseStatus {
+
+  public static final String SUCCESS = "success";
+  public static final String FAILURE = "failure";
+  public static final String ERROR = "error";
+}
