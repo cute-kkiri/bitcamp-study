452fb2d05e4d5409743276ab72c4a4b7e8ded7bd
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-21 12:18:40 +0900

    .

--- myapp/app-client/src/main/java/bitcamp/myapp/command/project/ProjectAddCommand.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/command/project/ProjectAddCommand.java
@@ -4,19 +4,19 @@ import bitcamp.command.Command;
 import bitcamp.myapp.dao.ProjectDao;
 import bitcamp.myapp.vo.Project;
 import bitcamp.util.Prompt;
-import java.sql.Connection;
+import org.apache.ibatis.session.SqlSession;
 
 public class ProjectAddCommand implements Command {
 
   private ProjectDao projectDao;
   private ProjectMemberHandler memberHandler;
-  private Connection con;
+  private SqlSession sqlSession;
 
   public ProjectAddCommand(ProjectDao projectDao, ProjectMemberHandler memberHandler,
-      Connection con) {
+                           SqlSession sqlSession) {
     this.projectDao = projectDao;
     this.memberHandler = memberHandler;
-    this.con = con;
+    this.sqlSession = sqlSession;
   }
 
   @Override
@@ -33,25 +33,16 @@ public class ProjectAddCommand implements Command {
       System.out.println("팀원:");
       memberHandler.addMembers(project);
 
-      con.setAutoCommit(false);
       projectDao.insert(project);
       projectDao.insertMembers(project.getNo(), project.getMembers());
-      con.commit();
+      sqlSession.commit();
 
       System.out.println("등록했습니다.");
 
     } catch (Exception e) {
-      try {
-        con.rollback();
-      } catch (Exception e2) {
-      }
+      sqlSession.rollback();
       System.out.println("등록 중 오류 발생!");
       e.printStackTrace();
-    } finally {
-      try {
-        con.setAutoCommit(true);
-      } catch (Exception e) {
-      }
     }
   }
 }
