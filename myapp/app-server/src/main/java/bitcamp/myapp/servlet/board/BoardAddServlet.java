f3cb83625bdb15fdacc5706b250532324194571c
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-28 12:58:43 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/servlet/board/BoardAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/servlet/board/BoardAddServlet.java
@@ -1,6 +1,7 @@
-package bitcamp.myapp.servlet.user;
+package bitcamp.myapp.servlet.board;
 
-import bitcamp.myapp.dao.UserDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.User;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -9,16 +10,16 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/user/add")
-public class UserAddServlet extends GenericServlet {
+@WebServlet("/board/add")
+public class BoardAddServlet extends GenericServlet {
 
-  private UserDao userDao;
+  private BoardDao boardDao;
   private SqlSessionFactory sqlSessionFactory;
 
   @Override
   public void init() throws ServletException {
     ServletContext ctx = this.getServletContext();
-    this.userDao = (UserDao) ctx.getAttribute("userDao");
+    this.boardDao = (BoardDao) ctx.getAttribute("boardDao");
     this.sqlSessionFactory = (SqlSessionFactory) ctx.getAttribute("sqlSessionFactory");
   }
 
@@ -31,30 +32,31 @@ public class UserAddServlet extends GenericServlet {
     out.println("<html>");
     out.println("<head>");
     out.println("    <meta charset='UTF-8'>");
-    out.println("    <meta http-equiv='refresh' content='1;url=/user/list'>");
+    out.println("    <meta http-equiv='refresh' content='1;url=/board/list'>");
     out.println("    <title>Title</title>");
     out.println("</head>");
     out.println("<body>");
 
     try {
-      out.println("<h1>회원 등록 결과</h1>");
-      
-      User user = new User();
-      user.setName(req.getParameter("name"));
-      user.setEmail(req.getParameter("email"));
-      user.setPassword(req.getParameter("password"));
-      user.setTel(req.getParameter("tel"));
+      out.println("<h1>게시글 등록 결과</h1>");
 
-      userDao.insert(user);
+      Board board = new Board();
+      board.setTitle(req.getParameter("title"));
+      board.setContent(req.getParameter("content"));
+      board.setWriter(new User(11));
+
+      boardDao.insert(board);
       sqlSessionFactory.openSession(false).commit();
       out.println("<p>등록 성공입니다.</p>");
 
     } catch (Exception e) {
       sqlSessionFactory.openSession(false).rollback();
       out.println("<p>등록 중 오류 발생!</p>");
+      e.printStackTrace();
     }
 
     out.println("</body>");
     out.println("</html>");
   }
+
 }
