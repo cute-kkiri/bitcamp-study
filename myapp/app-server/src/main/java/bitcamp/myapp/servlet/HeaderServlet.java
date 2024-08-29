aafc4ca2d0dc73d1a2239bd2a13c543b1934978b
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-29 16:14:21 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/servlet/HeaderServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/servlet/HeaderServlet.java
@@ -25,7 +25,15 @@ public class HeaderServlet extends GenericServlet {
 
     out.println("<header>");
     out.println("  <a href='/'><img src='/images/home.png'></a>");
-    out.println("        프로젝트 관리 시스템");
+    out.println("  <h1>프로젝트 관리 시스템</h1>");
+    out.println("  <nav>");
+    out.println("    <ul>");
+    out.println("      <li class='btn btn-default'><a href='/user/list'>회원</a></li>");
+    out.println("      <li class='btn btn-default'><a href='/project/list'>프로젝트</a></li>");
+    out.println("      <li class='btn btn-default'><a href='/board/list'>게시글</a></li>");
+    out.println("    </ul>");
+    out.println("  </nav>");
+    out.println("  <a href='/auth/form.html' class='btn btn-light pos-right'>로그인</a>");
     out.println("</header>");
   }
 }
