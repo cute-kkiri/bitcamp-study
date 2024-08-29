d5b3bf57a2de8e4e1072f82478fe6b859c46ed9a
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-29 15:21:28 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/servlet/project/ProjectViewServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/servlet/project/ProjectViewServlet.java
@@ -29,22 +29,11 @@ public class ProjectViewServlet extends GenericServlet {
   @Override
   public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
     res.setContentType("text/html;charset=UTF-8");
-
     PrintWriter out = res.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("    <meta charset='UTF-8'>");
-    out.println("    <title>Title</title>");
-    out.println("    <link href='/css/common.css' rel='stylesheet'>");
-    out.println("</head>");
-    out.println("<body>");
+
+    req.getRequestDispatcher("/header").include(req, res);
 
     try {
-      out.println("<header>");
-      out.println("  <a href='/'><img src='/images/home.png'></a>");
-      out.println("        프로젝트 관리 시스템");
-      out.println("</header>");
       out.println("<h1>프로젝트 조회</h1>");
 
       int projectNo = Integer.parseInt(req.getParameter("no"));
