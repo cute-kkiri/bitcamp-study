e0cbf0ca2667350449b6c41b6827363845a009c0
eomjinyoung <jinyoung.eom@gmail.com>
2024-09-06 16:24:55 +0900

    .

--- java-lang/app/src/main/java/TeamGenerator2.java
+++ java-lang/app/src/main/java/TeamGenerator2.java
@@ -5,11 +5,12 @@ public class TeamGenerator2 {
   public static void main(String[] args) throws Exception {
     Scanner keyboard = new Scanner(System.in);
 
+
     String[] students = {
-        "강슬기", "강윤상", "권기윤", "권준성", "김민수", "김재우", "김재정",
-        "김주연", "백현기", "신승민", "양지윤", "이가람", "이건학", "이민석",
-        "이선아", "이우성", "이재욱", "이태정", "임상우", "장혜정", "정찬우", "최동인",
-        "황민지"
+        //"강슬기", "권준성","정찬우", "최동인","임상우",
+        "강윤상", "권기윤", "김민수", "김재우", "김재정", "이건학",
+        "김주연", "백현기", "신승민", "양지윤", "이가람", "이민석",
+        "이선아", "이우성", "이재욱", "이태정", "장혜정", "황민지"
     };
 
     ArrayList<String> list = new ArrayList<>();
@@ -24,7 +25,7 @@ public class TeamGenerator2 {
     }
     System.out.println();
 
-    int s1 = 0, s2 = 0, s3 = 0;
+    int s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0, s6 = 0;
     while (list.size() > 1) {
       for (int i = 0; i < (int)(Math.random() * 100) + 1; i++) {
         s1 = (int) (Math.random() * list.size());
@@ -41,7 +42,23 @@ public class TeamGenerator2 {
       }
       String name3 = list.remove(s3);
 
-      System.out.printf("%s, %s, %s\n", name1, name2, name3);
+      for (int i = 0; i < (int)(Math.random() * 100) + 1; i++) {
+        s4 = (int) (Math.random() * list.size());
+      }
+      String name4 = list.remove(s4);
+
+      for (int i = 0; i < (int)(Math.random() * 100) + 1; i++) {
+        s5 = (int) (Math.random() * list.size());
+      }
+      String name5 = list.remove(s5);
+
+      for (int i = 0; i < (int)(Math.random() * 100) + 1; i++) {
+        s6 = (int) (Math.random() * list.size());
+      }
+      String name6 = list.remove(s6);
+
+
+      System.out.printf("%s, %s, %s, %s, %s, %s\n", name1, name2, name3, name4, name5, name6);
       keyboard.nextLine();
     }
 
