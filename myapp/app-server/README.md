227655fb8a1d03c17e406a917b09fb66d7f451cf
eomjinyoung <jinyoung.eom@gmail.com>
2024-09-06 15:36:19 +0900

    .

--- myapp/app-server/README.md
+++ myapp/app-server/README.md
@@ -9,6 +9,7 @@
 ## 요구사항
 
 - 로그인 할 때 입력한 이메일을 쿠키로 보관하고 꺼내기
+- 여러 페이지에 걸쳐서 프로젝트 정보를 입력 받기(세션 활용)
 
 ## 실행 결과
 
@@ -16,7 +17,12 @@
 
 ## 작업
 
-- 
+- 프로젝트 입력 폼 분리
+  - /project/form1.jsp, /project/form2.jsp, /project/form3.jsp 파일 추가
+  - ProjectForm1Servlet, ProjectForm2Servlet, ProjectForm3Servlet 클래스 생성
+  - form.jsp 파일 제거
+- 로그인 폼 변경
+  - /auth/form.jsp 변경
 
 ## 소스 파일
 
--- myapp/app-server/src/main/java/bitcamp/myapp/servlet/project/ProjectAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/servlet/project/ProjectAddServlet.java
@@ -1,9 +1,7 @@
 package bitcamp.myapp.servlet.project;
 
 import bitcamp.myapp.dao.ProjectDao;
-import bitcamp.myapp.dao.UserDao;
 import bitcamp.myapp.vo.Project;
-import bitcamp.myapp.vo.User;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import javax.servlet.ServletContext;
@@ -13,58 +11,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet("/project/add")
 public class ProjectAddServlet extends HttpServlet {
 
   private ProjectDao projectDao;
-  private UserDao userDao;
   private SqlSessionFactory sqlSessionFactory;
 
   @Override
   public void init() throws ServletException {
     ServletContext ctx = this.getServletContext();
     this.projectDao = (ProjectDao) ctx.getAttribute("projectDao");
-    this.userDao = (UserDao) ctx.getAttribute("userDao");
     this.sqlSessionFactory = (SqlSessionFactory) ctx.getAttribute("sqlSessionFactory");
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-    try {
-      List<User> users = userDao.list();
-      req.setAttribute("users", users);
-
-      res.setContentType("text/html;charset=UTF-8");
-      req.getRequestDispatcher("/project/form.jsp").include(req, res);
-
-    } catch (Exception e) {
-      req.setAttribute("exception", e);
-      req.getRequestDispatcher("/error.jsp").forward(req, res);
-    }
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
     try {
-      Project project = new Project();
-      project.setTitle(req.getParameter("title"));
-      project.setDescription(req.getParameter("description"));
-      project.setStartDate(Date.valueOf(req.getParameter("startDate")));
-      project.setEndDate(Date.valueOf(req.getParameter("endDate")));
-
-      String[] memberNos = req.getParameterValues("member");
-      if (memberNos != null) {
-        ArrayList<User> members = new ArrayList<>();
-        for (String memberNo : memberNos) {
-          members.add(new User(Integer.parseInt(memberNo)));
-        }
-        project.setMembers(members);
-      }
-
+      Project project = (Project) req.getSession().getAttribute("project");
       projectDao.insert(project);
 
       if (project.getMembers() != null && project.getMembers().size() > 0) {
@@ -72,6 +36,9 @@ public class ProjectAddServlet extends HttpServlet {
       }
       sqlSessionFactory.openSession(false).commit();
       res.sendRedirect("/project/list");
+
+      // 세션에 임시 보관했던 Project 객체를 제거한다.
+      req.getSession().removeAttribute("project");
 
     } catch (Exception e) {
       sqlSessionFactory.openSession(false).rollback();
