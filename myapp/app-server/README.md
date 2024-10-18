891209b3fc0f4dd8566e69469dcbaf33cdda1dd5
eomjinyoung <jinyoung.eom@gmail.com>
2024-10-18 14:51:55 +0900

    .

--- myapp/app-server/README.md
+++ myapp/app-server/README.md
@@ -1,17 +1,12 @@
-# 64. Spring WebMVC 활용하기
+# 65. SpringBoot(2.7.x) 적용하기
 
 ## 학습목표
 
-- @Controller와 @RequestMapping 애노테이션을 사용할 수 있다.
-- @PathVariable 애노테이션을 사용할 수 있다. 
-- @ResponseBody 애노테이션을 사용할 수 있다.
-- @ControllerAdvice 애노테이션을 사용할 수 있다.
-- @InitBinder, @ExceptionHandler 애노테이션을 사용할 수 있다.
-- 인터셉터를 사용할 수 있다.
+- Spring Boot를 프로젝트에 적용하고 설정할 수 있다.
 
 ## 요구사항
 
-- Spring WebMVC의 다양한 기법을 활용하라.
+- 기존의 Spring Framework 프로젝트를 Spring Boot 환경으로 설정하라.
 
 ## 실행 결과
 
@@ -19,17 +14,9 @@
 
 ## 작업
 
-- @Controller와 @RequestMapping
-  - 모든 페이지 컨트롤러에 적용
-- @PathVariable
-  - UserController에 적용
-- @ControllerAdvice
-  - GlobalControllerAdvice 클래스 생성
-- @InitBinder, @ExceptionHandler
-  - GlobalControllerAdvice에 적용
-- 인터셉터 적용
-  - AdminInterceptor 생성
-  - AppConfig 클래스 변경 
+- build.gradle 파일 변경
+- src/main/resources/application.properties 파일 생성
+- 설정에 따라 기타 파일을 변경하거나 삭제
 
 ## 소스 파일
 
--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -5,23 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @PropertySource("file:${user.home}/config/ncp.properties")
 @EnableTransactionManagement
-@Controller
 public class ServerApp implements WebMvcConfigurer {
 
   @Autowired
@@ -34,25 +25,6 @@ public class ServerApp implements WebMvcConfigurer {
 
   public static void main(String[] args) {
     SpringApplication.run(ServerApp.class, args);
-  }
-
-  @GetMapping("/hello")
-  @ResponseBody
-  public String hello() {
-    return "Hello!";
-  }
-
-  @Bean
-  public ViewResolver viewResolver() {
-    InternalResourceViewResolver vr = new InternalResourceViewResolver();
-    vr.setPrefix("/WEB-INF/jsp/");
-    vr.setSuffix(".jsp");
-    return vr;
-  }
-
-  @Bean
-  public MultipartResolver multipartResolver() {
-    return new StandardServletMultipartResolver();
   }
 
   @Override
